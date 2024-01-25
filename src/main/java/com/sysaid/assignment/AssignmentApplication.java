3aac586163f64c2ae3669ebe5cb44a0907bc3b37
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2024-01-25 21:11:11 +0100

    Initial commit

---
+++ src/main/java/com/sysaid/assignment/AssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.sysaid.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
+
+}
